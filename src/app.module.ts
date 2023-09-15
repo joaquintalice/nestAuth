c7d561b91ee3398b9f93eabccaedda356d9090e2
Joaquin Talice <joaquintalice@gmail.com>
2023-09-14 21:30:58 -0300

    feat: enable cors && create docker-compose to run DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './db/prisma.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/db/prisma.service';
+import AuthDto from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
 
-  async signup() {
-    return { message: 'just4test' };
+  constructor(private prismaService: PrismaService) { }
+
+  async signup(authDto: AuthDto) {
+    const { email, password } = authDto;
+
+
+
+    return { message: 'All data passed' };
   }
 
-  async signin() {}
+  async signin() { }
 
-  async signout() {}
+  async signout() { }
 }
